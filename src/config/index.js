388c1e25539ea37ae95ed4e04cf8e690ec763e4b
Angga Ady Pratama <46337871+anggaadypratama@users.noreply.github.com>
2021-08-19 16:45:05 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,3 @@
-// const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.REACT_APP_BASE_URL;
 const API_URL = process.env.REACT_APP_BASE_URL;
 
 export default {
