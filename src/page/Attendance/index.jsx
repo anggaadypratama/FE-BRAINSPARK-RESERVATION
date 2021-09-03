fbb06435565bda2fae01635e6f88da0895aa7da5
anggaadypratama <anggaadypratama@outlook.com>
2021-09-03 08:08:03 +0700

    refactor: adjust interval check autenticated

--- src/page/Attendance/index.jsx
+++ src/page/Attendance/index.jsx
@@ -58,6 +58,7 @@ const AttendancePage = () => {
 		() =>
 			getEventParticipantSearch(`${id}/absent/validate/`, {email: searchEmail})
 	);
+	
 	const mutation = useMutation(props =>
 		putEventParticipantAbsent(`${id}/absent`, props)
 	);
